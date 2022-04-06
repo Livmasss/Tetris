332205951a3ea96d34e1ae53b55a424f9ca714b8
Livmasss <seiranyan.sg@gmail.com>
2022-04-06 15:32:46 +0300

    V1.0.0

--- Assets/Scripts/PlayUI.cs
+++ Assets/Scripts/PlayUI.cs
@@ -1,17 +1,27 @@
 using UnityEngine.UIElements;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayUI : MonoBehaviour
 {
     public static Label score;
+    private Button mainMenu;
     void OnEnable()
     {
         var rootVisualElement = FindObjectOfType<UIDocument>().rootVisualElement;
         score = rootVisualElement.Q<Label>("Score");
+        mainMenu = rootVisualElement.Q<Button>("MainMenuButton");
+
+        mainMenu.RegisterCallback<ClickEvent>(ev => ToMainMenu());
     }
 
     void LateUpdate()
     {
         PlayUI.score.text = LoseAndScoring.score.ToString();
+    }
+
+    void ToMainMenu(){
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(0);
     }
 }
